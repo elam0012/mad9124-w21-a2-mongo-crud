58cabe067144f24dd22c6e35eae8c03f09776787
Yasser Ehaimer <yehaimer@Yassers-MacBook-Pro.local>
2022-03-09 14:25:13 -0500

    add get specific ID student  router

--- routers/studentsRouter.js
+++ routers/studentsRouter.js
@@ -16,12 +16,32 @@ studentsRouter.post('/', async (req, res) => {
   res.status(201).send({data: newStudent})
 })
 
-studentsRouter.get('/:id', async (req, res) => {})
+studentsRouter.get('/:id', async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id)
+    if (!student) throw new Error('Resource not found')
+    res.send({data: student})
+  } catch (err) {
+    sendResourceNotFound(req, res)
+  }
+})
 
 studentsRouter.patch('/:id', async (req, res) => {})
 
 studentsRouter.put('/:id', async (req, res) => {})
 
 studentsRouter.delete('/:id', async (req, res) => {})
+
+function sendResourceNotFound(req, res) {
+  res.status(404).send({
+    errors: [
+      {
+        status: '404',
+        title: 'Resource does not exist',
+        description: `We could not find a student with id: ${req.params.id}`
+      }
+    ]
+  })
+}
 
 module.exports = studentsRouter
